fa000e3e9b4f5d40ed836367d11bd84e4c7d978f
Gustavo Silva <gustavofsilva@email.com>
2020-02-26 19:41:46 -0300

    fixing the arrow function

--- main.js
+++ main.js
@@ -1,24 +1,4 @@
 const arr = [1,3,4,5,8,10];
 
-const newArr = arr.map((item, index) => {
-    return item + index; 
-})
-
-console.log(newArr);
-
-const sum = arr.reduce((total,next) => 
-    total + next
-);
-
-console.log(sum);
-
-const filter = arr.filter(item=>
-     item % 2 === 0
-)
-
-console.log(filter);
-
-const find = arr.find(item => item === 4)
-
-console.log(find);
+const newArr = arr.map((item, index) => item + index)
 
