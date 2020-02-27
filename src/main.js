a4c015d34fd07e4990dccde853611c06496a684c
Gustavo Silva <gustavofsilva@email.com>
2020-02-27 16:20:32 -0300

    create a aplication with ES6

--- src/main.js
+++ src/main.js
@@ -1,13 +1,73 @@
-import axios from 'axios';
+import api from './api';
 
-class Api {
-    static async getUserInfo(username){
-        const response = await axios.get(`https://api.github.com/users/${username}`);
-        console.log(response);
+class App {
+    constructor(){
+        this.repositories = [];
+
+        this.inpputEl = document.querySelector('input[name=repository]');
+        this.formEl = document.querySelector('#repo-form');
+        this.listEl = document.querySelector('#repo-list');
+        
+        this.registerHandlers();
     }
 
+    registerHandlers(){
+        this.formEl.onsubmit = event => this.addRepository(event);
+    }
 
+    async addRepository(){
+        event.preventDefault();
+
+        const repoInput = this.inpputEl.value;
+        
+        if(repoInput.length === 0)
+            return;
+
+        const response = await api.get(`/repos/${repoInput}`);
+        
+        const{name, description, html_url, owner: {avatar_url}} = response.data;
+        
+        console.log(response.data);
+        
+
+
+        this.repositories.push({
+            name,
+            description,
+            avatar_url,
+            html_url,
+        });
+
+        this.render();
+        
+    }
+
+    render(){
+        this.listEl.innerHTML = '';
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild(document.createTextNode(repo.description));
+            
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target', 'blank');
+            linkEl.setAttribute('href', repo.html_url)
+            linkEl.appendChild(document.createTextNode('Acessar Repositório'));
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+            
+            this.listEl.appendChild(listItemEl);
+        })
+    }
 }
 
-
-Api.getUserInfo('SilvaWT')
+new App();
