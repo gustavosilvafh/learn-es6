84c10fc578848809420c3d4d4d5fd8c40762ad5c
Gustavo Silva <gustavofsilva@email.com>
2020-02-27 14:39:49 -0300

    add dependences, polyfill and transform async

--- main.js
+++
@@ -1,6 +0,0 @@
-// import {soma, sub} from './funcoes';
-import soma from './soma';
-
-console.log(soma(1,2));
-
-
---
+++ src/main.js
@@ -0,0 +1,9 @@
+const minhaPromise = () => new Promise((resolve, reject) => {
+    setTimeout(() => {resolve('OK')}, 2000);
+});
+
+async function executaPromisse(){
+    const response = await minhaPromise();
+}
+
+executaPromisse();
